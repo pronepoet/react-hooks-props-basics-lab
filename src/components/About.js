13b9877cd9fdfb00e3b0986a77b3f487cf6722b3
pronepoet <ochiengphillip84@gmail.com>
2022-09-26 12:42:52 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Links from "./Links"
+function About(props) {
+  const isBio = props.bio
+  if (isBio){
+    return (
+      <div id="about">
+        <p>{props.bio}</p>
+        <h2>About Me</h2>
+        
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+       <Links linkedin ="https://github.com/liza" github="https://www.linkedin.com/in/liza/" /> 
+      </div>
+    );
+  }
+  else return null;
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+  
+
 }
 
 export default About;
